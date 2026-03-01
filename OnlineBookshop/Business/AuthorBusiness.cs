e8393b4cec8aee5426b9deb1884391db9de61a09
Georgi Grudev <ggrudev08@gmail.com>
2026-03-01 12:39:43 +0200

    Author business logic completed

--- OnlineBookshop/Business/AuthorBusiness.cs
+++ OnlineBookshop/Business/AuthorBusiness.cs
@@ -19,5 +19,48 @@ namespace OnlineBookshop.Business
             }
         }
 
+        public Author Get(int id)
+        {
+            using (authorContext = new OnlineBookshopContext())
+            {
+                return authorContext.Authors.Find(id);
+            }
+
+        }
+
+        public void Add(Author author)
+        {
+            using (authorContext = new OnlineBookshopContext())
+            {
+                authorContext.Authors.Add(author);
+                authorContext.SaveChanges();
+            }
+        }
+
+        public void Update(Author author)
+        {
+            using (authorContext = new OnlineBookshopContext())
+            {
+                var item = authorContext.Authors.Find(author.AuthorId);
+                if (item != null)
+                {
+                    authorContext.Entry(item).CurrentValues.SetValues(author);
+                    authorContext.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (authorContext = new OnlineBookshopContext())
+            {
+                var author = authorContext.Authors.Find(id);
+                if (author != null)
+                {
+                    authorContext.Authors.Remove(author);
+                    authorContext.SaveChanges();
+                }
+            }
+        }
     }
 }
