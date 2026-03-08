e1dc8761aecd221fc42172b3430331581b4268dc
ViktorLazarov77 <viktor.lazarov2008@gmail.com>
2026-03-08 18:46:34 +0200

    NUnit test display foulder created

---
+++ NUnit_Test_OnlineBookshop/Display/NUnit_Test_AuthorDisplay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_Test_OnlineBookshop.Display
+{
+    internal class NUnit_Test_AuthorDisplay
+    {
+    }
+}
