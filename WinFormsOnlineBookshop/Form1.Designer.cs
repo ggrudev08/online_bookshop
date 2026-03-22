7901ce0ff8bea99708d43cf73ec6620c7ee08000
Georgi Grudev <ggrudev08@gmail.com>
2026-03-22 13:39:11 +0200

    List all entries and fetch author working

--- WinFormsOnlineBookshop/Form1.Designer.cs
+++ WinFormsOnlineBookshop/Form1.Designer.cs
@@ -60,9 +60,9 @@
             HeadingLabel.Font = new Font("Magneto", 36F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             HeadingLabel.ForeColor = SystemColors.ControlText;
             HeadingLabel.ImageAlign = ContentAlignment.TopRight;
-            HeadingLabel.Location = new Point(325, 20);
+            HeadingLabel.Location = new Point(371, 27);
             HeadingLabel.Name = "HeadingLabel";
-            HeadingLabel.Size = new Size(462, 58);
+            HeadingLabel.Size = new Size(576, 73);
             HeadingLabel.TabIndex = 0;
             HeadingLabel.Text = "Online Bookshop";
             HeadingLabel.TextAlign = ContentAlignment.MiddleLeft;
@@ -72,10 +72,9 @@
             // 
             button1.BackColor = SystemColors.GradientActiveCaption;
             button1.Font = new Font("Impact", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
-            button1.Location = new Point(274, 402);
-            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Location = new Point(313, 536);
             button1.Name = "button1";
-            button1.Size = new Size(96, 31);
+            button1.Size = new Size(110, 41);
             button1.TabIndex = 1;
             button1.Text = "INSERT";
             button1.UseVisualStyleBackColor = false;
@@ -85,20 +84,19 @@
             // 
             comboBox1.BackColor = SystemColors.GradientActiveCaption;
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(273, 90);
-            comboBox1.Margin = new Padding(3, 2, 3, 2);
+            comboBox1.Location = new Point(312, 121);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(577, 23);
+            comboBox1.Size = new Size(659, 28);
             comboBox1.TabIndex = 2;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
             // 
             // button2
             // 
             button2.BackColor = SystemColors.GradientActiveCaption;
             button2.Font = new Font("Impact", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
-            button2.Location = new Point(578, 402);
-            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.Location = new Point(555, 536);
             button2.Name = "button2";
-            button2.Size = new Size(103, 30);
+            button2.Size = new Size(118, 40);
             button2.TabIndex = 3;
             button2.Text = "UPDATE";
             button2.UseVisualStyleBackColor = false;
@@ -107,10 +105,9 @@
             // 
             button3.BackColor = SystemColors.GradientActiveCaption;
             button3.Font = new Font("Impact", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
-            button3.Location = new Point(376, 402);
-            button3.Margin = new Padding(3, 2, 3, 2);
+            button3.Location = new Point(430, 536);
             button3.Name = "button3";
-            button3.Size = new Size(104, 30);
+            button3.Size = new Size(119, 40);
             button3.TabIndex = 4;
             button3.Text = "DELETE";
             button3.UseVisualStyleBackColor = false;
@@ -120,9 +117,9 @@
             label1.AutoSize = true;
             label1.Font = new Font("Impact", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 204);
             label1.ImageAlign = ContentAlignment.MiddleLeft;
-            label1.Location = new Point(28, 90);
+            label1.Location = new Point(32, 120);
             label1.Name = "label1";
-            label1.Size = new Size(88, 23);
+            label1.Size = new Size(109, 29);
             label1.TabIndex = 5;
             label1.Text = "Birth year";
             // 
@@ -131,9 +128,9 @@
             label2.AutoSize = true;
             label2.Font = new Font("Impact", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
             label2.ImageAlign = ContentAlignment.MiddleLeft;
-            label2.Location = new Point(28, 123);
+            label2.Location = new Point(32, 164);
             label2.Name = "label2";
-            label2.Size = new Size(93, 23);
+            label2.Size = new Size(116, 29);
             label2.TabIndex = 6;
             label2.Text = "First name";
             // 
@@ -142,9 +139,9 @@
             label3.AutoSize = true;
             label3.Font = new Font("Impact", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
             label3.ImageAlign = ContentAlignment.MiddleLeft;
-            label3.Location = new Point(28, 153);
+            label3.Location = new Point(32, 204);
             label3.Name = "label3";
-            label3.Size = new Size(90, 23);
+            label3.Size = new Size(112, 29);
             label3.TabIndex = 7;
             label3.Text = "Last name";
             // 
@@ -153,9 +150,9 @@
             label4.AutoSize = true;
             label4.Font = new Font("Impact", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
             label4.ImageAlign = ContentAlignment.MiddleLeft;
-            label4.Location = new Point(28, 183);
+            label4.Location = new Point(32, 244);
             label4.Name = "label4";
-            label4.Size = new Size(113, 23);
+            label4.Size = new Size(139, 29);
             label4.TabIndex = 8;
             label4.Text = "Release year";
             // 
@@ -163,16 +160,16 @@
             // 
             label5.Location = new Point(0, 0);
             label5.Name = "label5";
-            label5.Size = new Size(88, 17);
+            label5.Size = new Size(101, 23);
             label5.TabIndex = 11;
             // 
             // label6
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Impact", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
-            label6.Location = new Point(31, 217);
+            label6.Location = new Point(35, 289);
             label6.Name = "label6";
-            label6.Size = new Size(25, 23);
+            label6.Size = new Size(32, 29);
             label6.TabIndex = 10;
             label6.Text = "Id";
             label6.TextAlign = ContentAlignment.MiddleLeft;
@@ -180,77 +177,74 @@
             // txtBirthYear
             // 
             txtBirthYear.BackColor = SystemColors.GradientActiveCaption;
-            txtBirthYear.Location = new Point(146, 90);
-            txtBirthYear.Margin = new Padding(3, 2, 3, 2);
+            txtBirthYear.Location = new Point(169, 120);
             txtBirthYear.Name = "txtBirthYear";
-            txtBirthYear.Size = new Size(110, 23);
+            txtBirthYear.Size = new Size(125, 27);
             txtBirthYear.TabIndex = 12;
             // 
             // txtFirstName
             // 
             txtFirstName.BackColor = SystemColors.GradientActiveCaption;
-            txtFirstName.Location = new Point(146, 123);
-            txtFirstName.Margin = new Padding(3, 2, 3, 2);
+            txtFirstName.Location = new Point(169, 164);
             txtFirstName.Name = "txtFirstName";
-            txtFirstName.Size = new Size(110, 23);
+            txtFirstName.Size = new Size(125, 27);
             txtFirstName.TabIndex = 13;
             // 
             // txtLastName
             // 
             txtLastName.BackColor = SystemColors.GradientActiveCaption;
-            txtLastName.Location = new Point(146, 183);
-            txtLastName.Margin = new Padding(3, 2, 3, 2);
+            txtLastName.Location = new Point(169, 209);
             txtLastName.Name = "txtLastName";
-            txtLastName.Size = new Size(110, 23);
+            txtLastName.Size = new Size(125, 27);
             txtLastName.TabIndex = 14;
             // 
             // txtReleaseYear
             // 
             txtReleaseYear.BackColor = SystemColors.GradientActiveCaption;
-            txtReleaseYear.Location = new Point(146, 153);
-            txtReleaseYear.Margin = new Padding(3, 2, 3, 2);
+            txtReleaseYear.Location = new Point(169, 249);
             txtReleaseYear.Name = "txtReleaseYear";
-            txtReleaseYear.Size = new Size(110, 23);
+            txtReleaseYear.Size = new Size(125, 27);
             txtReleaseYear.TabIndex = 15;
             // 
             // txtId
             // 
             txtId.BackColor = SystemColors.GradientActiveCaption;
-            txtId.Location = new Point(146, 217);
-            txtId.Margin = new Padding(3, 2, 3, 2);
+            txtId.Location = new Point(169, 289);
             txtId.Name = "txtId";
-            txtId.Size = new Size(110, 23);
+            txtId.Size = new Size(125, 27);
             txtId.TabIndex = 16;
             // 
             // dataGridView1
             // 
             dataGridView1.BackgroundColor = SystemColors.GradientInactiveCaption;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(273, 134);
-            dataGridView1.Margin = new Padding(3, 2, 3, 2);
+            dataGridView1.Location = new Point(312, 179);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(578, 254);
+            dataGridView1.Size = new Size(661, 339);
             dataGridView1.TabIndex = 17;
             // 
             // btnAuthor
             // 
             btnAuthor.BackColor = SystemColors.GradientActiveCaption;
             btnAuthor.Font = new Font("Impact", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 204);
-            btnAuthor.Location = new Point(26, 268);
+            btnAuthor.Location = new Point(30, 357);
+            btnAuthor.Margin = new Padding(3, 4, 3, 4);
             btnAuthor.Name = "btnAuthor";
-            btnAuthor.Size = new Size(116, 34);
+            btnAuthor.Size = new Size(133, 45);
             btnAuthor.TabIndex = 18;
             btnAuthor.Text = "Author";
             btnAuthor.UseVisualStyleBackColor = false;
+            btnAuthor.Click += btnAuthor_Click;
             // 
             // btnBook
             // 
             btnBook.BackColor = SystemColors.GradientActiveCaption;
             btnBook.Font = new Font("Impact", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 204);
-            btnBook.Location = new Point(148, 308);
+            btnBook.Location = new Point(169, 411);
+            btnBook.Margin = new Padding(3, 4, 3, 4);
             btnBook.Name = "btnBook";
-            btnBook.Size = new Size(108, 36);
+            btnBook.Size = new Size(123, 48);
             btnBook.TabIndex = 19;
             btnBook.Text = "Book";
             btnBook.UseVisualStyleBackColor = false;
@@ -259,9 +253,10 @@
             // 
             btnCustomer.BackColor = SystemColors.GradientActiveCaption;
             btnCustomer.Font = new Font("Impact", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
-            btnCustomer.Location = new Point(26, 308);
+            btnCustomer.Location = new Point(30, 411);
+            btnCustomer.Margin = new Padding(3, 4, 3, 4);
             btnCustomer.Name = "btnCustomer";
-            btnCustomer.Size = new Size(116, 36);
+            btnCustomer.Size = new Size(133, 48);
             btnCustomer.TabIndex = 20;
             btnCustomer.Text = "Customer";
             btnCustomer.UseVisualStyleBackColor = false;
@@ -270,9 +265,10 @@
             // 
             btnOrder.BackColor = SystemColors.GradientActiveCaption;
             btnOrder.Font = new Font("Impact", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
-            btnOrder.Location = new Point(148, 268);
+            btnOrder.Location = new Point(169, 357);
+            btnOrder.Margin = new Padding(3, 4, 3, 4);
             btnOrder.Name = "btnOrder";
-            btnOrder.Size = new Size(108, 34);
+            btnOrder.Size = new Size(123, 45);
             btnOrder.TabIndex = 21;
             btnOrder.Text = "Order";
             btnOrder.UseVisualStyleBackColor = false;
@@ -282,19 +278,21 @@
             // 
             btnExit.BackColor = SystemColors.GradientActiveCaption;
             btnExit.Font = new Font("Impact", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
-            btnExit.Location = new Point(486, 402);
+            btnExit.Location = new Point(679, 536);
+            btnExit.Margin = new Padding(3, 4, 3, 4);
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(86, 30);
+            btnExit.Size = new Size(98, 40);
             btnExit.TabIndex = 22;
             btnExit.Text = "EXIT";
             btnExit.UseVisualStyleBackColor = false;
+            btnExit.Click += btnExit_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(863, 454);
+            ClientSize = new Size(986, 605);
             Controls.Add(btnExit);
             Controls.Add(btnOrder);
             Controls.Add(btnCustomer);
@@ -317,7 +315,6 @@
             Controls.Add(comboBox1);
             Controls.Add(button1);
             Controls.Add(HeadingLabel);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
--- WinFormsOnlineBookshop/Form1.cs
+++ WinFormsOnlineBookshop/Form1.cs
@@ -1,7 +1,17 @@
+using OnlineBookshop.Business;
+using OnlineBookshop.Data.Models;
+
 namespace WinFormsOnlineBookshop
 {
     public partial class Form1 : Form
     {
+        private AuthorBusiness authorBusiness = new AuthorBusiness();
+        private BookBusiness bookBusiness = new BookBusiness();
+        private CustomerBusiness customerBusiness = new CustomerBusiness();
+        private OrderBusiness orderBusiness = new OrderBusiness();
+
+        private int flag = -1;
+
         public Form1()
         {
             InitializeComponent();
@@ -17,6 +27,88 @@ namespace WinFormsOnlineBookshop
 
         }
 
-       
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnAuthor_Click(object sender, EventArgs e)
+        {
+            flag = 1;
+            ClearTextBoxes();
+
+            //Set the unused labels/textboxes to invisible
+            label1.Visible = true;
+            txtBirthYear.Visible = true;
+
+            label2.Visible = true;
+            txtFirstName.Visible = true;
+
+            label3.Visible = true;
+            txtLastName.Visible = true;
+
+            label4.Hide();
+            txtReleaseYear.Hide();
+
+            label5.Visible = true;
+            txtId.Visible = true;
+
+            //Console.WriteLine("1. All authors");
+            //Console.WriteLine("2. Add new author");
+            //Console.WriteLine("3. Update");
+            //Console.WriteLine("4. Fetch author by ID");
+            //Console.WriteLine("5. Delete author by ID");
+            //Console.WriteLine("6. Exit");
+
+
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("List all entries.");
+            comboBox1.Items.Add("Fetch entry by ID");
+            comboBox1.Items.Add("Exit");
+        }
+
+        private void ClearTextBoxes()
+        {
+            txtBirthYear.Text = "";
+            txtLastName.Text = "";
+            txtFirstName.Text = "";
+            txtReleaseYear.Text = "0";
+            txtId.Text = "0";
+        }
+
+        private void UpdateGridAuthor()
+        {
+            dataGridView1.DataSource = authorBusiness.GetAll();
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private void AuthorShow()
+        {
+            UpdateGridAuthor();
+            ClearTextBoxes();
+            
+        }
+
+        private void FetchAuthor()
+        {
+            int id = int.Parse(txtId.Text);
+            dataGridView1.DataSource = new List<Author> { authorBusiness.Get(id) };
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            ClearTextBoxes();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (flag == 1)
+            {
+                if(comboBox1.SelectedIndex == 0)
+                    AuthorShow();
+                else if (comboBox1.SelectedIndex == 1)
+                    FetchAuthor();
+            }
+        }
     }
 }
